26dc5318e0d9a2b8b963aa0bdf756cb50897613d
Anjana Nair <anju740@gmail.com>
2019-10-18 04:03:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -78,7 +78,7 @@ class TicTacToe
     win_index_2 = combo[1]
     win_index_3 = combo[2]
 
-    if position_taken?(board,win_index_1) && position_taken?(board,win_index_2) && position_taken?(board,win_index_3)
+    if position_taken?(win_index_1) && position_taken?(win_index_2) && position_taken?(win_index_3)
       if (board[win_index_1] == "X" && board[win_index_2] == "X" && board[win_index_3] == "X") || (board[win_index_1] == "O" && board[win_index_2] == "O" && board[win_index_3] == "O")
         return combo
       end
