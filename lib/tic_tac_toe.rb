6956d11db7bafce14ae905cebd8da1ea9af2a8e3
Anjana Nair <anju740@gmail.com>
2019-10-18 03:59:52 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,78 @@
+class TicTacToe
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  def initialize
+    @board = Array.new(9, " ")
+  end
+
+  def display_board
+    print " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    print "-----------"
+    print " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    print "-----------"
+    print " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(index)
+    index.to_i - 1
+  end
+
+  def move(index, token = "X")
+    @board[index] = token
+  end
+
+  def position_taken?(index)
+    if @board[index] == "X" || @board[index] == "O"
+      true
+    else
+      false
+    end
+  end
+
+  def valid_move?(position)
+    if position_taken?(position) | !@board[position]
+      false
+    else
+      true
+    end
+  end
+
+  def turn
+    input = gets
+    input = input_to_index(input)
+    if valid_move?(input)
+      move(input, token = "X")
+      display_board
+    else
+      turn
+    end
+  end
+
+  def turn_count
+    count = 0
+    @board.each do |position|
+      if position == "X" || position == "O"
+        count += 1
+      end
+    end
+    count
+  end
+
+  def current_player
+    if turn_count % 2 == 0
+      "O"
+    else
+      "X"
+    end
+  end
+end
